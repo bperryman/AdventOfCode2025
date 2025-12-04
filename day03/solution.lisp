ca8d93c128ba163d1eccfa201f6b2aa5c137d3f3
Barry Perryman <barry.perryman@gmail.com>
2025-12-04 14:52:59 +0000

    Day 3 - part 2 solution
    Create a better way of finding the largest number possible.
    Rework the 1st solution to use it as well.

--- day03/solution.lisp
+++ day03/solution.lisp
@@ -1,52 +1,32 @@
 (defun load-data (file)
   (aoc:read-data-file file))
 
-(defun largest-2-digit-number (str)
-  (loop for c across str
-        for i from 1 below (length str)
-        maximize (loop for next-c across (subseq str i)
-                       maximize (parse-integer (format nil "~c~c" c next-c)))))
+(defun largest-digit-in-string (str &optional (start 0) (end (1- (length str))))
+  "Return the digit and position of the largest digit possible from the string"
+  (let ((highest #\0)
+        (highest-index start))
+    (loop for i from start to end
+          when (char> (char str i) highest)
+            do (setf highest-index i
+                     highest (char str i))
+          finally (return (values highest highest-index)))))
+
+(defun largest-n-digit-number (str n)
+  (let* ((len (length str))
+         (start 0)
+         (end (- len n))
+         (return-string (make-string n :initial-element #\0)))
+    (dotimes (i n (parse-integer return-string))
+      (multiple-value-bind (digit index) (largest-digit-in-string str start end)
+        (setf (char return-string i) digit
+              start (1+ index)
+              end (1+ end))))))
 
 (defun solution-1 (file)
   (let ((nums (load-data file)))
     (loop for batteries in nums
-          sum (largest-2-digit-number batteries))))
+          sum (largest-n-digit-number batteries 2))))
 
-#|
-
-(defun largest-n-digit-number (str n)
-  "Returns the largest n digit number taken from str without sorting the digits."
-  (cond
-    ((< (length str) n)
-     (error "Not enough digits in string ~a to retrieve ~a digits" str n))
-    ((= n 0) "")
-    ((= (length str) n)
-     (parse-integer str))
-    (t (loop for digit across str
-             for i from 1 to (- (length str) n)
-             maximize (max (parse-integer
-                            (format nil "~c~a"
-                                    digit
-                                    (largest-n-digit-number (subseq str 1) (- n 1))))
-                           (largest-n-digit-number (subseq str 1) n))))))
-
-
-
-|#
-
-(defun remove-lowest-digit (digit-string)
-  (let ((lowest-digit (char (sort (copy-seq digit-string) #'char<) 0)))
-    (remove lowest-digit digit-string :count 1)))
-
-(defun largest-n-digit-number (str n)
-  (let ((len (length str)))
-    (do ((digit-string (subseq str (- len n)))
-         (i (- len n 1) (1- i)))
-        ((= i -1) (parse-integer digit-string))
-      (when (char<= (char digit-string 0) (char str i))
-        (setf digit-string (format nil "~c~a" (char str i) (remove-lowest-digit digit-string)))))))
-
-;; This function isn't quite right, so I'm not getting the correct answer - the one I'm getting is too low.
 (defun solution-2 (file)
   (let ((nums (load-data file)))
     (loop for batteries in nums
